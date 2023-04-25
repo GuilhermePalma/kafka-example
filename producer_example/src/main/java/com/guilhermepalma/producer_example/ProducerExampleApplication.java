2303bca37600841b5ae1fc67fe50d5f50f8ef582
Guilherme Palma <guippalma@gmail.com>
2023-04-25 12:20:19 -0300

    feat: add configurations

--- producer_example/src/main/java/com/guilhermepalma/producer_example/ProducerExampleApplication.java
+++ producer_example/src/main/java/com/guilhermepalma/producer_example/ProducerExampleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableKafka // Set Kafka in Spring Boot Project
 public class ProducerExampleApplication {
 
 	public static void main(String[] args) {
