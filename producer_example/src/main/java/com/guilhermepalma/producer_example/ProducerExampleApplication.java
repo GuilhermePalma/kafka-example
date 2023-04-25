ec7c45a94b925aac0df8a655614621e5a8d3f213
Guilherme Palma <guippalma@gmail.com>
2023-04-25 14:17:24 -0300

    fix: import libary to @EnableKafka

--- producer_example/src/main/java/com/guilhermepalma/producer_example/ProducerExampleApplication.java
+++ producer_example/src/main/java/com/guilhermepalma/producer_example/ProducerExampleApplication.java
@@ -2,13 +2,14 @@ package com.guilhermepalma.producer_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka // Set Kafka in Spring Boot Project
 public class ProducerExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerExampleApplication.class, args);
+    }
 
 }
