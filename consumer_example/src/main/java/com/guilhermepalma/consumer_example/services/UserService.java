d59b7dcc1423ab0d0e7a292abf324c101db72423
Guilherme Palma <guippalma@gmail.com>
2023-04-25 18:00:39 -0300

    fix: set group-id

--- consumer_example/src/main/java/com/guilhermepalma/consumer_example/services/UserService.java
+++ consumer_example/src/main/java/com/guilhermepalma/consumer_example/services/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    @KafkaListener(topics = "${mykafka.topics.change-user}")
+    @KafkaListener(topics = "${mykafka.topics.change-user}", groupId = "group-id")
     private void consumeEvents(ConsumerRecord<String, User> event) {
         log.info(String.format("Topic: [%s], Key: [%s], Headers: [%s], Payload: [ %s ]", event.topic(), event.key(),
                 event.headers(), event.value()));
