788277aad24f564d7e6dfabfcfc5890db0288b09
Guilherme Palma <guippalma@gmail.com>
2023-04-25 12:11:17 -0300

    feat: create initial projects structs

---
+++ consumer_example/src/main/java/com/guilhermepalma/consumer_example/ConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermepalma.consumer_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerExampleApplication.class, args);
+	}
+
+}
---
+++ producer_example/src/main/java/com/guilhermepalma/producer_example/ProducerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermepalma.producer_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerExampleApplication.class, args);
+	}
+
+}
