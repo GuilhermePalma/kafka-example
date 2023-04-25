4b583763e3db27e6ed9b3bf85d2894418d49f63b
Guilherme Palma <guippalma@gmail.com>
2023-04-25 18:01:12 -0300

    refact: customize return when happens errors

--- producer_example/src/main/java/com/guilhermepalma/producer_example/endpoints/UserController.java
+++ producer_example/src/main/java/com/guilhermepalma/producer_example/endpoints/UserController.java
@@ -3,13 +3,10 @@ package com.guilhermepalma.producer_example.endpoints;
 import com.guilhermepalma.producer_example.model.User;
 import com.guilhermepalma.producer_example.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +20,7 @@ public class UserController {
             id = userService.createUser(user);
             return ResponseEntity.ok(id);
         } catch (Exception ex) {
-            return ResponseEntity.internalServerError().body("Internal error. ID: " + id);
+            return ResponseEntity.internalServerError().body("Internal error. ID: " + ex);
         }
     }
 
