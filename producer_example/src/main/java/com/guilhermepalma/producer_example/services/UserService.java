54a20d57e751bd310815843f521446a95e982675
Guilherme Palma <guippalma@gmail.com>
2023-04-25 14:27:42 -0300

    fix: remove final parameter

--- producer_example/src/main/java/com/guilhermepalma/producer_example/services/UserService.java
+++ producer_example/src/main/java/com/guilhermepalma/producer_example/services/UserService.java
@@ -3,7 +3,6 @@ package com.guilhermepalma.producer_example.services;
 import com.guilhermepalma.producer_example.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -15,16 +14,15 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final KafkaTemplate<String, String> kafkaTemplate;
     @Value("${mykafka.topics.change-user}")
-    private final String CREATE_USER_TOPIC;
+    private String CREATE_USER_TOPIC;
 
-    private final KafkaTemplate<String, User> kafkaTemplate;
-
-    public String createUser(User user){
+    public String createUser(User user) {
         String idTransaction = UUID.randomUUID().toString();
 
         log.info(String.format("ID Transaction: [%s]\nPayload: [ %s ]", idTransaction, user.toString()));
-        kafkaTemplate.send(CREATE_USER_TOPIC, idTransaction, user);
+        kafkaTemplate.send(CREATE_USER_TOPIC, idTransaction, user.toString());
 
         return idTransaction;
     }
