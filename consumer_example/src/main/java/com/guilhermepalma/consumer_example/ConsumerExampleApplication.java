74ad1cd4d112751a380e101f18f963ffd8321139
Guilherme Palma <guippalma@gmail.com>
2023-05-24 10:19:46 -0300

    feat: enable async operations

--- consumer_example/src/main/java/com/guilhermepalma/consumer_example/ConsumerExampleApplication.java
+++ consumer_example/src/main/java/com/guilhermepalma/consumer_example/ConsumerExampleApplication.java
@@ -3,13 +3,15 @@ package com.guilhermepalma.consumer_example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableKafka // Set Kafka in Spring Boot Project
+@EnableAsync // Enable Async in Spring Boot
 public class ConsumerExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerExampleApplication.class, args);
+    }
 
 }
