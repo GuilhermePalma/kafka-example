7dd94f9442a6789a7d29f8397b9e6ebe22237e75
Guilherme Palma <guippalma@gmail.com>
2023-05-24 10:17:41 -0300

    feat: add parallelism in consumer topic
    using Kafka annotations is possible paralelize the consumer process

--- consumer_example/src/main/java/com/guilhermepalma/consumer_example/services/UserService.java
+++ consumer_example/src/main/java/com/guilhermepalma/consumer_example/services/UserService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +13,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    @KafkaListener(topics = "${mykafka.topics.change-user}", groupId = "group-id")
-    private void consumeEvents(ConsumerRecord<String, User> event) {
+    @KafkaListener(topics = "${mykafka.topics.change-user}", groupId = "group-id", concurrency = "5",
+            topicPartitions = @TopicPartition(
+                    topic = "${mykafka.topics.change-user}", partitions = {"0", "1", "2", "3", "4"}))
+    private void consumeEvents(ConsumerRecord<String, User> event) throws InterruptedException {
+        log.info("start now");
+        Thread.sleep(10000);
         log.info(String.format("Topic: [%s], Key: [%s], Headers: [%s], Payload: [ %s ]", event.topic(), event.key(),
                 event.headers(), event.value()));
+        log.info("finish now");
     }
 
 }
